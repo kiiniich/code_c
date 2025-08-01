44b0e534dc57c00106ce91545eba81bc37db6224
kiiniich <15081742813@163.com>
2025-08-01 14:58:46 +0800

    hhh

---
+++ test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	printf("第一个：");
+       int scanf = ("%d\n", num1);
+	printf("第二个：");
+		scanf("%d\n", num2);
+	int sum = num1 + num2;
+	printf("总和：%d", sum);
+
+
+		return 0;
+}
