92373f69f1d08627b18cbd6d52908fd41de91c99
kiiniich <15081742813@163.com>
2025-08-26 16:51:53 +0800

    Update game.c

--- game.c
+++ game.c
@@ -9,17 +9,19 @@ void menu()
 {
 	printf("*****************************\n");
 	printf("*****  输入 1 开始游戏  *****\n");
+	printf("*****  输入 2 进入设置  *****\n");
 	printf("*****  输入 0 退出游戏  *****\n");
 	printf("*****************************\n");
 }
 
 
-void game()
+void game(int max)
 {
 	int guess = 0;
+	int i = 0;
 	//生成随机数
 	int ret = rand()%100+1;
-	while (1)
+	for(i = 0; i < max; i++)
 	{
 		printf("请猜数字：");
 		scanf("%d",&guess);
@@ -33,13 +35,17 @@ void game()
 			break;
 		}
 	}
-	
+	if(i == max)
+	printf("%d次未猜对，游戏结束！\n",max);
+	Sleep(1000);
 }
+
 
 int main()
 {
 	srand((unsigned int)time(NULL));
 	int input = 0;
+	int cishu = 3;
 
 	do
 	{
@@ -48,8 +54,17 @@ int main()
 		scanf("%d", &input);
 		switch (input)
 		{
+		case 2:
+			printf("请输入你想猜测的次数（默认是3次）：");
+			scanf("%d", &cishu);
+			printf("设置成功！\n");
+			break;
 		case 1:
-			game();
+			printf("游戏即将开始!\n");
+			Sleep(2000);
+			printf("你只有%d次猜的机会。\n",cishu);
+			Sleep(2000);
+			game(cishu);
 			Sleep(1000);
 			printf("即将返回菜单\n");
 			Sleep(2100);
